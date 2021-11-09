6d7144d4f41b80f04b43d95e28c8afea39a296cd
connorcarnes <connorcarnes44@gmail.com>
2021-11-08 20:52:05 -0600

    Increase logging in Package-PythonServerless.ps1

--- CodeBuild/UnitTestAndBuild/Package-PythonServerless.ps1
+++ CodeBuild/UnitTestAndBuild/Package-PythonServerless.ps1
@@ -11,12 +11,17 @@ if ([String]::IsNullOrWhiteSpace($env:ARTIFACT_S3_BUCKET)) {
     throw 'The environment variable ARTIFACT_S3_BUCKET must be configured.'
 }
 
-# When executing in CodeBuild, this file is executed from the root of the CodeBuild environment.
-# Setting this to a variable to assist with path generation
+Write-Host("codeBuildRoot: When executing in CodeBuild, this file is executed from the root of the CodeBuild environment. " +
+    "Setting this to a variable to assist with path generation."
 $codeBuildRoot = $env:CODEBUILD_SRC_DIR
+Write-Host"codeBuildRoot is: $($codeBuildRoot)"
 
 $pythonLambdaRoot = [System.IO.Path]::Combine($codeBuildRoot, 'lambdafunctions', 'python')
+Write-Host"pythonLambdaRoot is $pythonLambdaRoot"
+
 $lambdaHandlerSourceFiles = [System.IO.Path]::Combine($pythonLambdaRoot, 'src', '*.py')
+Write-Host"lambdaHandlerSourceFiles is $lambdaHandlerSourceFiles"
+Write-Host $($lambdaHandlerSourceFiles | format-table -AutoSize | Out-string)
 
 # $cfnServerlessApiPath = [System.IO.Path]::Combine($codeBuildRoot, 'cloudformation', 'apc-api')
 # $lambdaSourceDestinationPath = [System.IO.Path]::Combine($cfnServerlessApiPath, 'src')
@@ -35,6 +40,8 @@ Copy-Item -Path $lambdaHandlerSourceFiles -Destination $cfnCodeUriPath -Force
 $folders = Get-ChildItem -Path $pythonLambdaRoot -Directory | Where-Object {
     $_.Name -notin ('__pycache__', 'src', 'tests')
 }
+Write-Host $($folders | format-table -AutoSize | Out-string)
+
 foreach ($folder in $folders) {
     # Create the destination folder
     # $destinationPath = [System.IO.Path]::Combine($lambdaSourceDestinationPath, $folder.Name)
@@ -44,3 +51,5 @@ foreach ($folder in $folders) {
     $sourceFiles = [System.IO.Path]::Combine($folder.FullName, '*.py')
     Copy-Item -Path $sourceFiles -Destination $destinationPath -Force -Exclude '__pycache__'
 }
+
+Write-Host $(gci $cfnCodeUriPath -recurse | format-table -AutoSize | Out-string)
