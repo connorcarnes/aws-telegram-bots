3ffea6ae1c5faeab8f7bab21672db1ca09d48c8f
connorcarnes <connorcarnes44@gmail.com>
2021-12-03 17:25:17 -0600

    Update how webhook var is assigned in set_webhooks.ps1

--- CodeBuild/IntegrationTest/set_webhooks.ps1
+++ CodeBuild/IntegrationTest/set_webhooks.ps1
@@ -4,15 +4,14 @@
 )
 
 foreach ($Bot in $Bots) {
+    Write-Host "Setting $Bot webhook..."
+    $Webhook = [Environment]::GetEnvironmentVariable(($Bot + 'Webhook'))
     $CipherText = (Get-SSMParameter -Name "/tgbots/$Bot").value
     $EncryptedBytes = [System.Convert]::FromBase64String($CipherText)
     $EncryptedMemory = New-Object System.IO.MemoryStream($EncryptedBytes, 0, $EncryptedBytes.Length)
     $DecryptedMemory = Invoke-KMSDecrypt -CiphertextBlob $EncryptedMemory
     $BotValues = ConvertFrom-Json ([System.Text.Encoding]::UTF8.GetString($DecryptedMemory.Plaintext.ToArray()))
-    $Uri = "https://api.telegram.org/bot$($BotValues.BOT_TOKEN)/setWebhook?url=$($ENV:FleetMorselBotWebhook)"
-
-    Write-Host "Setting $Bot webhook to $Uri"
-
+    $Uri = "https://api.telegram.org/bot$($BotValues.BOT_TOKEN)/setWebhook?url=$Webhook"
     Invoke-RestMethod -Uri $Uri
 }
 
