a966fa6859d8d3051da64b971de06c1535a6b7b6
connorcarnes <connorcarnes44@gmail.com>
2021-12-03 14:07:18 -0600

    Update set_webhooks.ps1 to set new bot webhooks

--- CodeBuild/IntegrationTest/set_webhooks.ps1
+++ CodeBuild/IntegrationTest/set_webhooks.ps1
@@ -9,3 +9,16 @@ $Uri = "https://api.telegram.org/bot$($BotValues.BOT_TOKEN)/setWebhook?url=$($EN
 write-host "Setting webhook to $($Uri)"
 
 Invoke-RestMethod -Uri $Uri
+
+
+$CipherText = (Get-SSMParameter -Name '/tgbots/loquaciousechobot').value
+$EncryptedBytes = [System.Convert]::FromBase64String($cipherText)
+$EncryptedMemoryStreamToDecrypt = New-Object System.IO.MemoryStream($encryptedBytes, 0, $encryptedBytes.Length)
+$DecryptedMemoryStream = Invoke-KMSDecrypt -CiphertextBlob $encryptedMemoryStreamToDecrypt
+$BotValues = ConvertFrom-Json ([System.Text.Encoding]::UTF8.GetString($decryptedMemoryStream.Plaintext.ToArray()))
+
+$Uri = "https://api.telegram.org/bot$($BotValues.BOT_TOKEN)/setWebhook?url=$($ENV:LoquaciousEchoBotWebhook)"
+
+write-host "Setting webhook to $($Uri)"
+
+Invoke-RestMethod -Uri $Uri
